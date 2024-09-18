a1b7dd577203c704cbc89167a3f5a4f940547ad4
RepubblicaTech <elshorbagy.omar@gozzi-olivetti.org>
2024-09-18 19:23:07 +0200

    Commenting code is important

--- src/include/gdt.c
+++ src/include/gdt.c
@@ -29,12 +29,13 @@ void reload_segments() {
 	);
 }
 
+// https://wiki.osdev.org/GDT_Tutorial#Flat_/_Long_Mode_Setup
 void gdt_init() {
-    gdt.gdt_entries[0] = (gdt_entry_t){0, 0, 0, 0, 0, 0};
-	gdt.gdt_entries[1] = (gdt_entry_t){0, 0, 0, 0x9A, 0xA0, 0};
-	gdt.gdt_entries[2] = (gdt_entry_t){0, 0, 0, 0x92, 0xA0, 0};
-	gdt.gdt_entries[3] = (gdt_entry_t){0, 0, 0, 0xFA, 0xA0, 0};
-	gdt.gdt_entries[4] = (gdt_entry_t){0, 0, 0, 0xF2, 0xA0, 0};
+    gdt.gdt_entries[0] = (gdt_entry_t){0, 0, 0, 0, 0, 0};			// Null Segment
+	gdt.gdt_entries[1] = (gdt_entry_t){0, 0, 0, 0x9A, 0xA0, 0};		// 64-bit kernel code segment
+	gdt.gdt_entries[2] = (gdt_entry_t){0, 0, 0, 0x92, 0xA0, 0};		// 64-bit kernel data segment
+	gdt.gdt_entries[3] = (gdt_entry_t){0, 0, 0, 0xFA, 0xA0, 0};		// 64-bit user code segment
+	gdt.gdt_entries[4] = (gdt_entry_t){0, 0, 0, 0xF2, 0xA0, 0};		// 64-bit user data segment
 
     gdtr.size = (uint16_t)(sizeof(gdt) - 1);
     gdtr.pointer = (gdt_entry_t*)&gdt;
