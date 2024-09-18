0d51c659b43a320eca662c66e18e13abecdd4cd9
RepubblicaTech <elshorbagy.omar@gozzi-olivetti.org>
2024-09-18 19:25:52 +0200

    Basic CPU functions
    Might come in handy for later ;)

---
+++ src/include/util/cpu.c
@@ -0,0 +1,25 @@
+#include "cpu.h"
+
+#include <cpuid.h>
+#include <util/pic.h>
+
+/* Get CPU's model number */
+static int get_model(void) {
+    int ebx, unused;
+    __cpuid(0, unused, ebx, unused, unused);
+    return ebx;
+}
+
+bool check_msr() {
+   static uint32_t a, unused, d; // eax, edx
+   __get_cpuid(1, &a, &unused, &unused, &d);
+   return d & CPUID_FEAT_EDX_MSR;
+}
+
+void cpu_get_msr(uint32_t msr, uint32_t *lo, uint32_t *hi) {
+   asm volatile("rdtsc" : "=a"(*lo), "=d"(*hi) : "c"(msr));
+}
+
+void cpu_set_msr(uint32_t msr, uint32_t lo, uint32_t hi) {
+   asm volatile("wrmsr" : : "a"(lo), "d"(hi), "c"(msr));
+}
