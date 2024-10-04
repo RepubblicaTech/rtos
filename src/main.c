ca9d5777ae38490a618e6ffe8465eca71771d460
RepubblicaTech <elshorbagy.omar@gozzi-olivetti.org>
2024-10-04 21:00:17 +0200

    Move all base.h contents to main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,10 @@
-#include <util/base.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#include <flanterm/flanterm.h>
+#include <flanterm/backends/fb.h>
+
 #include <util/memory.h>
 
 #include <stdio.h>
@@ -8,7 +14,40 @@
 #include <isr.h>
 #include <irq.h>
 
+#include <limine.h>
+
 #include <util/cpu.h>
+
+/*
+    Set the base revision to 2, this is recommended as this is the latest
+    base revision described by the Limine boot protocol specification.
+    See specification for further info.
+
+    --- DON'T PUT THIS ANYWHERE ELSE OTHER THAN HERE. ---
+*/
+
+__attribute__((used, section(".requests")))
+static volatile LIMINE_BASE_REVISION(2);
+
+// The Limine requests can be placed anywhere, but it is important that
+// the compiler does not optimise them away, so, usually, they should
+// be made volatile or equivalent, _and_ they should be accessed at least
+// once or marked as used with the "used" attribute as done here.
+
+__attribute__((used, section(".requests")))
+static volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0
+};
+
+// Finally, define the start and end markers for the Limine requests.
+// These can also be moved anywhere, to any .c file, as seen fit.
+
+__attribute__((used, section(".requests_start_marker")))
+static volatile LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".requests_end_marker")))
+static volatile LIMINE_REQUESTS_END_MARKER;
 
 struct limine_framebuffer *framebuffer;
 struct flanterm_context *ft_ctx;
