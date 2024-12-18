9e9f90b1c9d9030e97e57e9a8eb9718c38ee55ec
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2024-12-18 19:44:08 +0100

    Rename _panic to _hcf

--- src/kernel/include/assert.c
+++ src/kernel/include/assert.c
@@ -2,12 +2,12 @@
 
 #include <stdio.h>
 
-extern void _panic();
+extern void _hcf();
 
 void assert_impl(const char *function, int line, bool condition, char *condtion_str) {
 	if (!condition) {
 		kprintf("--- [ PANIC @ %s():%d ] --- Condition <%s> failed!\n", function, line, condtion_str);
 
-		_panic();
+		_hcf();
 	}
 }
