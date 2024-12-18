6181fcf1e294dcee24dd3723fbbf0095bf1115e1
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2024-12-18 19:44:46 +0100

    RSDP & MADT tables parsing

---
+++ src/kernel/include/acpi/rsdp.c
@@ -0,0 +1,21 @@
+#include "rsdp.h"
+
+#include <kernel.h>
+
+#include <util/string.h>
+
+static struct bootloader_data limine_data;
+extern void _hcf();
+
+int is_xsdp(sdt_pointer* pointer) {
+	return (pointer->revision > 0);
+}
+
+// returns the root system table pointer
+sdt_pointer* get_rsdp() {
+	// we need the address that points to the RSDP
+	limine_data = get_bootloader_data();
+	uint64_t* v_rsdp_addr = (uint64_t*)limine_data.rsdp_table_address;
+
+	return (sdt_pointer*)v_rsdp_addr;
+}
---
+++ src/kernel/include/acpi/tables/madt.h
@@ -0,0 +1,65 @@
+#ifndef MADT_H
+#define MADT_H 1
+
+#include <acpi/acpi.h>
+
+typedef struct madt_record {
+	uint8_t entry_type;
+	uint8_t entry_length;
+} madt_record;
+
+#define MADT_ENTRY_LAPIC				0
+#define MADT_ENTRY_IOAPIC				1
+#define MADT_ENTRY_IOAPIC_INT_OVERRIDE	2
+#define MADT_ENTRY_NMI_SOURCE			3
+#define MADT_ENTRY_LAPIC_ADDR_OVVERRIDE	5		// it's the 64-bit address of the LAPIC
+#define MADT_ENTRY_LAPIC_NMI			4
+#define MADT_ENTRY_LOCAL_X2APIC			9
+
+typedef struct madt_lapic {
+	madt_record record;
+	uint8_t acpi_processor_id;
+	uint8_t id;
+	uint32_t flags;
+} madt_lapic;
+
+typedef struct madt_ioapic
+{
+	madt_record record;
+	uint8_t id;
+	uint8_t reserved;
+	uint32_t address;
+	uint32_t global_sys_interrupt_base;
+} madt_ioapic;
+
+typedef struct madt_ioapic_int_override
+{	
+	madt_record record;
+	uint8_t bus_source;
+	uint8_t legacy_irq_source;
+	uint32_t ioapic_irq;
+	uint16_t flags;
+
+} madt_ioapic_int_override;
+
+typedef struct madt_lapic_addr_ovveride {
+	madt_record record;
+	uint16_t reserved;
+	uint64_t physical_base;					// physical 64-bit address of the LAPIC
+} madt_lapic_addr_ovveride;
+
+typedef struct madt {
+	acpi_sdt_header header;
+
+	uint32_t lapic_control_address;
+	uint32_t lapic_flags;
+
+	uint8_t* entries;
+} madt;
+
+madt* get_madt();
+
+void madt_devices_init();
+madt_record* find_record(madt* madt_h, const uint8_t entry_type);
+
+#endif
