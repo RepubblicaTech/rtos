cf6213017f49e66d60b3bb386615da712bfc87de
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2024-12-18 19:40:16 +0100

    CHange enums to defines and move them to pic.h

--- src/arch/x86_64/pic.c
+++ src/arch/x86_64/pic.c
@@ -3,53 +3,9 @@
 
 
 void pic_sendEOI(uint8_t irq) {
-	if(irq >= 8) outb(PIC2_COMMAND, PIC_EOI);
-	outb(PIC1_COMMAND, PIC_EOI);
+	if(irq >= 8) _outb(PIC2_COMMAND, PIC_EOI);
+	_outb(PIC1_COMMAND, PIC_EOI);
 }
-
-// Initialization Control Word 1
-// -----------------------------
-//  0   IC4     if set, the PIC expects to receive ICW4 during initialization
-//  1   SGNL    if set, only 1 PIC in the system; if unset, the PIC is cascaded with slave PICs
-//              and ICW3 must be sent to controller
-//  2   ADI     call address interval, set: 4, not set: 8; ignored on x86, set to 0
-//  3   LTIM    if set, operate in level triggered mode; if unset, operate in edge triggered mode
-//  4   INIT    set to 1 to initialize PIC
-//  5-7         ignored on x86, set to 0
-
-enum {
-    PIC_ICW1_ICW4           = 0x01,
-    PIC_ICW1_SINGLE         = 0x02,
-    PIC_ICW1_INTERVAL4      = 0x04,
-    PIC_ICW1_LEVEL          = 0x08,
-    PIC_ICW1_INITIALIZE     = 0x10
-} PIC_ICW1;
-
-
-// Initialization Control Word 4
-// -----------------------------
-//  0   uPM     if set, PIC is in 80x86 mode; if cleared, in MCS-80/85 mode
-//  1   AEOI    if set, on last interrupt acknowledge pulse, controller automatically performs 
-//              end of interrupt operation
-//  2   M/S     only use if BUF is set; if set, selects buffer master; otherwise, selects buffer slave
-//  3   BUF     if set, controller operates in buffered mode
-//  4   SFNM    specially fully nested mode; used in systems with large number of cascaded controllers
-//  5-7         reserved, set to 0
-enum {
-    PIC_ICW4_8086           = 0x1,
-    PIC_ICW4_AUTO_EOI       = 0x2,
-    PIC_ICW4_BUFFER_MASTER  = 0x4,
-    PIC_ICW4_BUFFER_SLAVE   = 0x0,
-    PIC_ICW4_BUFFERRED      = 0x8,
-    PIC_ICW4_SFNM           = 0x10,
-} PIC_ICW4;
-
-
-enum {
-    PIC_CMD_END_OF_INTERRUPT    = 0x20,
-    PIC_CMD_READ_IRR            = 0x0A,
-    PIC_CMD_READ_ISR            = 0x0B,
-} PIC_CMD;
 
 /*
 arguments:
@@ -59,34 +15,34 @@ arguments:
 */
 void pic_config(int offset1, int offset2) {
 	// uint8_t a1, a2;
-	
+
 	// a1 = inb(PIC1_DATA);                                        // save masks
 	// a2 = inb(PIC2_DATA);
 	
-	outb(PIC1_COMMAND, PIC_ICW1_INITIALIZE | PIC_ICW1_ICW4);    // starts the initialization sequence (in cascade mode)
-	io_wait();
-	outb(PIC2_COMMAND, PIC_ICW1_INITIALIZE | PIC_ICW1_ICW4);
-	io_wait();
+	_outb(PIC1_COMMAND, PIC_ICW1_INITIALIZE | PIC_ICW1_ICW4);    // starts the initialization sequence (in cascade mode)
+	_io_wait();
+	_outb(PIC2_COMMAND, PIC_ICW1_INITIALIZE | PIC_ICW1_ICW4);
+	_io_wait();
 
-	outb(PIC1_DATA, offset1);                                   // ICW2: Master PIC vector offset
-	io_wait();
-	outb(PIC2_DATA, offset2);                                   // ICW2: Slave PIC vector offset
-	io_wait();
+	_outb(PIC1_DATA, offset1);                                   // ICW2: Master PIC vector offset
+	_io_wait();
+	_outb(PIC2_DATA, offset2);                                   // ICW2: Slave PIC vector offset
+	_io_wait();
 
-	outb(PIC1_DATA, 0x04);                                      // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
-	io_wait();
-	outb(PIC2_DATA, 0x02);                                      // ICW3: tell Slave PIC its cascade identity (0000 0010)
-	io_wait();
+	_outb(PIC1_DATA, 0x04);                                      // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+	_io_wait();
+	_outb(PIC2_DATA, 0x02);                                      // ICW3: tell Slave PIC its cascade identity (0000 0010)
+	_io_wait();
 	
-	outb(PIC1_DATA, PIC_ICW4_8086);                             // ICW4: have the PICs use 8086 mode (and not 8080 mode)
-	io_wait();
-	outb(PIC2_DATA, PIC_ICW4_8086);
-	io_wait();
+	_outb(PIC1_DATA, PIC_ICW4_8086);                             // ICW4: have the PICs use 8086 mode (and not 8080 mode)
+	_io_wait();
+	_outb(PIC2_DATA, PIC_ICW4_8086);
+	_io_wait();
 	
-	outb(PIC1_DATA, 0);   // restore saved masks.
-    io_wait();
-	outb(PIC2_DATA, 0);
-    io_wait();
+	_outb(PIC1_DATA, 0);
+    _io_wait();
+	_outb(PIC2_DATA, 0);
+    _io_wait();
 }
 
 void irq_mask(uint8_t irq_line) {
@@ -99,8 +55,8 @@ void irq_mask(uint8_t irq_line) {
         port = PIC2_DATA;
         irq_line -= 8;
     }
-    value = inb(port) | (1 << irq_line);
-    outb(port, value);
+    value = _inb(port) | (1 << irq_line);
+    _outb(port, value);
 }
 
 void irq_unmask(uint8_t irq_line) {
@@ -113,26 +69,45 @@ void irq_unmask(uint8_t irq_line) {
         port = PIC2_DATA;
         irq_line -= 8;
     }
-    value = inb(port) & ~(1 << irq_line);
-    outb(port, value);        
+    value = _inb(port) & ~(1 << irq_line);
+    _outb(port, value);        
 }
 
 // Disable the PIC
 void pic_disable() {
+    _outb(PIC1_COMMAND, PIC_ICW1_INITIALIZE | PIC_ICW1_ICW4);
+    _io_wait();
+    _outb(PIC2_COMMAND, PIC_ICW1_INITIALIZE | PIC_ICW1_ICW4);
+    _io_wait();
+
+    _outb(PIC1_DATA, 0x20);
+    _io_wait();
+    _outb(PIC1_DATA, 0x28);
+    _io_wait();
+
+    _outb(PIC1_DATA, 0x2);
+    _io_wait();
+    _outb(PIC1_DATA, 0x4);
+    _io_wait();
+
+    _outb(PIC1_DATA, PIC_ICW4_8086);
+    _io_wait();
+    _outb(PIC1_DATA, PIC_ICW4_8086);
+    _io_wait();
+
     // Masks all interrupts
-    outb(PIC1_DATA, 0xff);
-    io_wait();
-    outb(PIC2_DATA, 0xff);
-    io_wait();
+    _outb(PIC1_DATA, 0xff);
+    _io_wait();
+    _outb(PIC2_DATA, 0xff);
 }
 
 /* Helper func */
 static uint16_t __pic_get_irq_reg(int ocw3) {
     /* OCW3 to PIC CMD to get the register values.  PIC2 is chained, and
      * represents IRQs 8-15.  PIC1 is IRQs 0-7, with 2 being the chain */
-    outb(PIC1_COMMAND, ocw3);
-    outb(PIC2_COMMAND, ocw3);
-    return (inb(PIC2_COMMAND) << 8) | inb(PIC1_COMMAND);
+    _outb(PIC1_COMMAND, ocw3);
+    _outb(PIC2_COMMAND, ocw3);
+    return (_inb(PIC2_COMMAND) << 8) | _inb(PIC1_COMMAND);
 }
 
 /* Returns the combined value of the cascaded PICs irq request register */
