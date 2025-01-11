be0172e90b0d8c25671363c630f2858e61a815db
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2025-01-11 15:20:29 +0100

    Setting up GDT fields properly

--- src/arch/x86_64/gdt.c
+++ src/arch/x86_64/gdt.c
@@ -4,30 +4,27 @@
 #include "gdt.h"
 
 gdt_pointer_t gdtr;
-
-struct __attribute__((packed)) {
-    gdt_entry_t gdt_entries[5];
-} gdt;
+gdt_entry_t gdt_entries[5];
 
 extern void _load_gdt(gdt_pointer_t* descriptor);
 extern void _reload_segments(uint64_t cs, uint64_t ds);
 
 // https://wiki.osdev.org/GDT_Tutorial#Flat_/_Long_Mode_Setup
 void gdt_init() {
-    gdt.gdt_entries[0] = (gdt_entry_t){0, 0, 0, 0, 0, 0};			// Null Segment
-	gdt.gdt_entries[1] = (gdt_entry_t){0, 0, 0, 0x9A, 0xA0, 0};		// 64-bit kernel code segment
-	gdt.gdt_entries[2] = (gdt_entry_t){0, 0, 0, 0x92, 0xA0, 0};		// 64-bit kernel data segment
-	gdt.gdt_entries[3] = (gdt_entry_t){0, 0, 0, 0xFA, 0xA0, 0};		// 64-bit user code segment
-	gdt.gdt_entries[4] = (gdt_entry_t){0, 0, 0, 0xF2, 0xA0, 0};		// 64-bit user data segment
+    gdt_entries[0] = (gdt_entry_t)GDT_ENTRY(0, 0, 0, 0);			// Null Segment
+	gdt_entries[1] = (gdt_entry_t)GDT_ENTRY(0, 0xFFFFF, 0x9A, 0xA);		// 64-bit kernel code segment
+	gdt_entries[2] = (gdt_entry_t)GDT_ENTRY(0, 0xFFFFF, 0x92, 0xC);		// 64-bit kernel data segment
+	gdt_entries[3] = (gdt_entry_t)GDT_ENTRY(0, 0xFFFFF, 0xFA, 0xA);		// 64-bit user code segment
+	gdt_entries[4] = (gdt_entry_t)GDT_ENTRY(0, 0xFFFFF, 0xF2, 0xC);		// 64-bit user data segment
 
-    gdtr.size = (uint16_t)(sizeof(gdt) - 1);
-    gdtr.pointer = (gdt_entry_t*)&gdt;
+    gdtr.size = (uint16_t)(sizeof(gdt_entries) - 1);
+    gdtr.pointer = (gdt_entry_t*)&gdt_entries;
 
 	debugf_debug("GDTR:\n");
 	debugf_debug("\tsize: %u\n", gdtr.size);
-	debugf_debug("\tpointer: %p\n", gdtr.pointer);
+	debugf_debug("\tpointer: %llp\n", gdtr.pointer);
 
-	debugf_debug("Loading GDTR %#llx\n", *(uint64_t*)&gdtr);
+	debugf_debug("Loading GDTR %llp\n", (uint64_t*)&gdtr);
     _load_gdt(&gdtr);
 
 	_reload_segments(GDT_CODE_SEGMENT, GDT_DATA_SEGMENT);
--- src/arch/x86_64/include/gdt.h
+++ src/arch/x86_64/include/gdt.h
@@ -6,13 +6,29 @@
 #define GDT_CODE_SEGMENT 0x08
 #define GDT_DATA_SEGMENT 0x10
 
+// Helper macros
+#define GDT_LIMIT_LOW(limit)                (limit & 0xFFFF)
+#define GDT_BASE_LOW(base)                  (base & 0xFFFF)
+#define GDT_BASE_MIDDLE(base)               ((base >> 16) & 0xFF)
+#define GDT_FLAGS_HI_LIMIT(limit, flags)    (((limit >> 16) & 0xF) | ((flags << 4) & 0xF0))
+#define GDT_BASE_HIGH(base)                 ((base >> 24) & 0xFF)
+
+#define GDT_ENTRY(base, limit, access, flags) {\
+    GDT_LIMIT_LOW(limit),                      \
+    GDT_BASE_LOW(base),                        \
+    GDT_BASE_MIDDLE(base),                     \
+    access,                                    \
+    GDT_FLAGS_HI_LIMIT(limit, flags),          \
+    GDT_BASE_HIGH(base)                        \
+}
+
 typedef struct {
-    uint16_t limit_low;
-    uint16_t base_low;
-    uint8_t base_middle;
-    uint8_t access;
-    uint8_t limit_high_and_flags;
-    uint8_t base_high;
+    uint16_t limit_low;                     // limit & 0xFF
+    uint16_t base_low;                      // base & 0xFF
+    uint8_t base_middle;                    // (base >> 16) & 0xFF
+    uint8_t access;                         // access
+    uint8_t limit_high_and_flags;           // ((limit >> 16) & 0xF) | (flags & 0xF0)
+    uint8_t base_high;                      // (base >> 24) & 0xF
 } __attribute__((packed)) gdt_entry_t;
 
 typedef struct {
