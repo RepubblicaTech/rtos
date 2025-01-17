6f9714b7a9dd7457b2d81c78085fc96dc480c76b
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2025-01-17 22:02:17 +0100

    VMA update: allocate pages instead of bytes

--- src/kernel/memory/vma.c
+++ src/kernel/memory/vma.c
@@ -9,7 +9,7 @@
 #include "util/util.h"
 
 // @param map_allocation Tells the allocator if the found region needs to be mapped (disable for kernel VMA, enable for other VMAs)
-void* vma_alloc(vmm_context_t* ctx, size_t size, bool map_allocation) {
+void* vma_alloc(vmm_context_t* ctx, size_t pages, bool map_allocation) {
 	void* ptr = NULL;
 
 	virtmem_object_t* cur_vmo = ctx->root_vmo;
@@ -17,24 +17,32 @@ void* vma_alloc(vmm_context_t* ctx, size_t size, bool map_allocation) {
 
 	for (; cur_vmo != NULL; cur_vmo = cur_vmo->next)
 	{	
-		debugf_debug("Checking for available memory\n");
-		vmo_dump(cur_vmo);
+		#ifdef VMM_DEBUG
+			debugf_debug("Checking for available memory\n");
+			vmo_dump(cur_vmo);
+		#endif
 
-		if ((cur_vmo->flags & VMO_ALLOCATED) || cur_vmo->len < size) {
-			debugf_debug("Current VMO is either too small or already allocated. Skipping...\n");
-
+		if ((cur_vmo->flags & VMO_ALLOCATED) || cur_vmo->len < pages) {
+			#ifdef VMM_DEBUG
+				debugf_debug("Current VMO is either too small or already allocated. Skipping...\n");
+			#endif
 			if (cur_vmo->next == NULL) {
-				new_vmo = vmo_init(cur_vmo->base + (uint64_t)cur_vmo->len,
-								ROUND_UP(size, PMLT_SIZE),
-								cur_vmo->flags & 0xFF);
+				new_vmo = vmo_init(cur_vmo->base + (uint64_t)(cur_vmo->len * PMLT_SIZE),
+								 pages,
+								  cur_vmo->flags & 0xFF);
 				cur_vmo->next = new_vmo;
-				debugf_debug("VMO %p created successfully. Proceeding to next iteration\n", new_vmo);
+				new_vmo->prev = cur_vmo;
+				#ifdef VMM_DEBUG
+					debugf_debug("VMO %p created successfully. Proceeding to next iteration\n", new_vmo);
+				#endif
 			}
 
 			continue;
 		}
-
-		debugf_debug("Well, we've got enough memory :D\n");
+		
+		#ifdef VMM_DEBUG
+			debugf_debug("Well, we've got enough memory :D\n");
+		#endif
 		break;
 	}
 
@@ -43,35 +51,47 @@ void* vma_alloc(vmm_context_t* ctx, size_t size, bool map_allocation) {
 		_hcf();
 	}
 
-	cur_vmo = split_vmo_at(cur_vmo, size);
+	cur_vmo = split_vmo_at(cur_vmo, pages);
 	cur_vmo->flags |= VMO_ALLOCATED;
 
 	ptr = (void*)(cur_vmo->base);
 	if (map_allocation) {
-		void* phys = fl_alloc(size);
-		map_region_to_page(ctx->pml4_table, (uint64_t)phys, (uint64_t)ptr, (uint64_t)size, vmo_to_page_flags(cur_vmo->flags));
+		void* phys = fl_alloc(pages * PMLT_SIZE);
+		map_region_to_page(ctx->pml4_table, (uint64_t)phys, (uint64_t)ptr, (uint64_t)(pages * PMLT_SIZE), vmo_to_page_flags(cur_vmo->flags));
 	} else {
-		debugf_debug("No need to map the region\n");
+		#ifdef VMM_DEBUG
+			debugf_debug("No need to map the region\n");
+		#endif
 	}
 
-	debugf_debug("Returning pointer %p\n", ptr);
+	#ifdef VMM_DEBUG
+		debugf_debug("Returning pointer %p\n", ptr);
+	#endif
 	return ptr;
 }
 
 void vma_free(vmm_context_t* ctx, void* ptr, bool unmap_allocation) {
-	debugf_debug("Deallocating pointer %p\n", ptr);
+	#ifdef VMM_DEBUG
+		debugf_debug("Deallocating pointer %p\n", ptr);
+	#endif
 
 	virtmem_object_t* cur_vmo = ctx->root_vmo;
 	for (; cur_vmo != NULL; cur_vmo = cur_vmo->next) {
-		vmo_dump(cur_vmo);
+		#ifdef VMM_DEBUG
+			vmo_dump(cur_vmo);
+		#endif
 		if ((uint64_t)ptr == cur_vmo->base) {
 			break;
 		}
-		debugf_debug("Pointer and vmo->base don't match. Skipping\n");
+		#ifdef VMM_DEBUG
+			debugf_debug("Pointer and vmo->base don't match. Skipping\n");
+		#endif
 	}
 
 	if (cur_vmo == NULL) {
-		debugf_debug("Tried to deallocate a non-existing pointer. Quitting...\n");
+		#ifdef VMM_DEBUG
+			debugf_debug("Tried to deallocate a non-existing pointer. Quitting...\n");
+		#endif
 		return;
 	}
 
@@ -81,6 +101,8 @@ void vma_free(vmm_context_t* ctx, void* ptr, bool unmap_allocation) {
 		fl_free((void*)pg_virtual_to_phys(ctx->pml4_table, cur_vmo->base));
 		unmap_region(ctx->pml4_table, cur_vmo->base, cur_vmo->len);
 	} else {
-		debugf_debug("We don't have to unmap\n");
+		#ifdef VMM_DEBUG
+			debugf_debug("We don't have to unmap\n");
+		#endif
 	}
 }
