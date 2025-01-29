9b590b45e26ad019e2c701996cdf6c2f8892ca12
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2025-01-29 14:17:49 +0100

    Add `memmap_response` structure to Limine responses structs

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -101,7 +101,8 @@ struct limine_framebuffer *framebuffer;
 struct flanterm_context *ft_ctx;
 struct flanterm_fb_context *ft_fb_ctx;
 
-struct limine_memmap_entry *entry;
+struct limine_memmap_response *memmap_response;
+struct limine_memmap_entry *memmap_entry;
 struct limine_hhdm_response *hhdm_response;
 struct limine_paging_mode_response *paging_mode_response;
 struct limine_kernel_address_response *kernel_address_response;
