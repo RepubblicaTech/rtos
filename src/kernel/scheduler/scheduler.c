d6b00e0eb352043e772210f171e669ac21d75e32
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2025-02-06 12:21:06 +0100

    Actual working scheduler :D

--- src/kernel/scheduler/scheduler.c
+++ src/kernel/scheduler/scheduler.c
@@ -18,7 +18,7 @@
 
 #include <spinlock.h>
 
-uint64_t pid_counter;
+size_t procs_counter;
 uint64_t current_pid; // which process is currently running
 process_t **processes = NULL;
 
@@ -30,6 +30,8 @@ atomic_flag SCHEDULER_LOCK;
 
 // to create a process, you need to give it some kind of starting point
 process_t *create_process(void (*entry)()) {
+    spinlock_acquire(&SCHEDULER_LOCK);
+
     _load_pml4(get_kernel_pml4());
 
     process_t *process = kmalloc(sizeof(process_t));
@@ -37,31 +39,34 @@ process_t *create_process(void (*entry)()) {
     process->pml4 = (uint64_t *)PHYS_TO_VIRTUAL(pmm_alloc_page());
     pagemap_copy_to(process->pml4);
 
-    registers_t cpu_frame;
-    process->registers_frame.ds = GDT_DATA_SEGMENT;
+    memset(&process->registers_frame, 0, sizeof(registers_t));
+    // process->registers_frame.ds = GDT_DATA_SEGMENT;
+    process->registers_frame.ss = GDT_DATA_SEGMENT;
     process->registers_frame.cs = GDT_CODE_SEGMENT;
     // the stack grows downwards :^)
     process->registers_frame.rsp =
-        (uint64_t)(PHYS_TO_VIRTUAL(pmm_alloc_page()) + (PFRAME_SIZE - 1));
+        (uint64_t)(PHYS_TO_VIRTUAL(pmm_alloc_pages(4)) +
+                   ((PFRAME_SIZE * 4) - 1));
+    process->registers_frame.rbp    = 0;
     process->registers_frame.rip    = (uint64_t)entry;
     process->registers_frame.rflags = 0x202;
 
-    process->pid = pid_counter;
+    process->pid = procs_counter;
 
     process->time_slice = SCHED_TIME_SLICE;
     process->state      = PROC_STATUS_NEW;
-    process->pid        = pid_counter;
+    process->pid        = procs_counter++;
 
     // process->prev = NULL;
     // process->next = NULL;
 
     // append the process to the list
-    processes[pid_counter] = process;
-
-    pid_counter++;
+    processes[procs_counter - 1] = process;
 
     // debugf_debug("Process (PID: %llu, entry:%#llx) has been created\n",
     //              process->pid, process->registers_frame.rip);
+
+    spinlock_release(&SCHEDULER_LOCK);
 
     return process;
 }
@@ -79,21 +84,15 @@ void process_handler(registers_t *cur_registers_frame) {
         return;
 
     // _load_pml4(get_kernel_pml4());
+
     process_t *current_process = processes[current_pid];
     if (!current_process)
         return;
 
-    // time slice is <= 0
-    if (current_process->time_slice <= 0) {
-        current_process->time_slice = SCHED_TIME_SLICE;
-        current_pid                 = ++current_pid % pid_counter;
-        current_process             = processes[current_pid];
-    }
-
     if (current_process->state == PROC_STATUS_RUNNING &&
         current_process->time_slice > 0) {
         current_process->time_slice--;
-        memcpy(&processes[current_pid]->registers_frame, cur_registers_frame,
+        memcpy(&current_process->registers_frame, cur_registers_frame,
                sizeof(registers_t));
 
         return;
@@ -102,10 +101,17 @@ void process_handler(registers_t *cur_registers_frame) {
         current_process->state = PROC_STATUS_RUNNING;
     }
 
-    // here, we should have an existing next
-    memcpy(cur_registers_frame, &processes[current_pid]->registers_frame,
+    // time slice is <= 0
+    if (current_process->time_slice <= 0) {
+        current_process->time_slice = SCHED_TIME_SLICE;
+        current_pid                 = ++current_pid % procs_counter;
+        current_process             = processes[current_pid];
+        current_process->state      = PROC_STATUS_NEW;
+    }
+
+    memcpy(cur_registers_frame, &current_process->registers_frame,
            sizeof(registers_t));
-    _load_pml4((uint64_t *)VIRT_TO_PHYSICAL(processes[current_pid]->pml4));
+    _load_pml4((uint64_t *)VIRT_TO_PHYSICAL(current_process->pml4));
 }
 
 void scheduler_init() {
@@ -114,8 +120,8 @@ void scheduler_init() {
         processes[i] = NULL;
     }
 
-    pid_counter = 0;
-    current_pid = 0;
+    procs_counter = 0;
+    current_pid   = 0;
 
     irq_registerHandler(0, sched_timer_tick);
 }
