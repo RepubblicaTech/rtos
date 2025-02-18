710ac25c32dc134b59ae15f35d53fd8614b955b0
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2025-02-18 15:18:31 +0100

    change `oct2bin` implementation

--- src/kernel/util/util.c
+++ src/kernel/util/util.c
@@ -1,12 +1,14 @@
 #include "util.h"
 
 int oct2bin(const char *str, int size) {
-    int n            = 0;
-    unsigned char *c = str;
-    while (size-- > 0) {
+    int n = 0;
+    char c;
+
+    for (int i = 0; i < size; i++) {
+        c  = str[i];
         n *= 8;
-        n += *c - '0';
-        c++;
+        n += (c - '0');
     }
+
     return n;
 }
