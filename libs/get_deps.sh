484cab3e055cdd28191bb0c9b3016142064341ec
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2025-03-15 09:07:10 +0100

    Comments are useful

--- libs/get_deps.sh
+++ libs/get_deps.sh
@@ -39,13 +39,15 @@ copy_if_exists() {
 		return 1
 	fi
 
+	# parse everything after last slash
 	LAST_SLASH=${2##*/}
 	if [[ $LAST_SLASH == *"."* ]]; then
-		DIRECTORY=${2//$LAST_SLASH}
+		DIRECTORY=${2//$LAST_SLASH}	# remove the last part if it's a file...
 	else
 		DIRECTORY=$2
 	fi
 
+	# ...to create the parent directory
 	if [ ! -d "$DIRECTORY" ]; then
 		stderr_echo "target directory $DIRECTORY not found. Creating it..."
 		mkdir -p "$DIRECTORY"
@@ -53,7 +55,7 @@ copy_if_exists() {
 
 	echo "--> Copying: $1"
 	echo "	  to: $2"
-	cp -f "$1" "$2"
+	cp -fr "$1" "$2"
 }
 
 if [ -z "$KERNEL_DIR" ]; then
@@ -99,3 +101,13 @@ patch -su $KERNEL_DIR/flanterm/backends/fb.c -i $LIBS_DIR/patches/fb.c.patch >/d
 patch -su $KERNEL_DIR/memory/heap/liballoc.h -i $LIBS_DIR/patches/liballoc.h.patch >/dev/null 2>&1
 patch -su $KERNEL_DIR/memory/heap/liballoc.c -i $LIBS_DIR/patches/liballoc.c.patch >/dev/null 2>&1
 
+# copy uACPI
+# NOTE: very wacky, must make it better
+copy_if_exists $LIBS_DIR/uacpi/include/uacpi $KERNEL_DIR/acpi
+UACPI_BASE_PATH="$LIBS_FULL_DIR/uacpi"
+for uacpi_c in "$UACPI_BASE_PATH/source/"*; do
+    if [[ "$uacpi_c" == *".c" ]]; then
+        # printf "$uacpi_c\n"
+        copy_if_exists $uacpi_c $KERNEL_DIR/acpi/uacpi
+    fi
+done
