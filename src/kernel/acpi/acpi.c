69f8374b2b2dc0b973ff9bd5115661181702dfdf
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2025-04-19 22:31:10 +0200

    Change uACPI init flags

--- src/kernel/acpi/acpi.c
+++ src/kernel/acpi/acpi.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 int uacpi_init() {
-    uacpi_status ret = uacpi_initialize(UACPI_FLAG_NO_OSI);
+    uacpi_status ret = uacpi_initialize(0);
     if (uacpi_likely_error(ret)) {
         uacpi_kernel_log(UACPI_LOG_ERROR,
                          "Couldn't initialize basic ACPI components!\n");
