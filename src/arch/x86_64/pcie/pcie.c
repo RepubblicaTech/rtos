20854f6117ccbf421db17a0b2c3ab1da31ed2b73
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2025-06-16 21:31:10 +0200

    Initial PCIe devices view implementation
    It just lists available PCIe devices (i think)

--- src/arch/x86_64/pcie/pcie.c
+++ src/arch/x86_64/pcie/pcie.c
@@ -6,7 +6,37 @@
 #include <uacpi/tables.h>
 #include <uacpi/uacpi.h>
 
+#include <paging/paging.h>
+
 #include <stdio.h>
+
+void dump_pcie_info(void *pcie_addr) {
+    pcie_header_t *pcie_header = pcie_addr;
+
+    mprintf("\t%hx:%hx (rev %.02d) (header type %02d)\n",
+            pcie_header->vendor_id, pcie_header->device_id,
+            pcie_header->revision_id, pcie_header->header_type);
+    mprintf("\tMemory AND I/O requests: %sSUPPORTED\n",
+            pcie_header->command_reg & (1 << 2) ? "NOT " : "");
+
+    void *p = (pcie_addr + sizeof(pcie_header_t));
+
+    switch (pcie_header->header_type) {
+    case PCIE_HEADER_T0:
+        pcie_header0_t *header0 = p;
+
+        for (int i = 0; i < 6; i++) {
+            mprintf("\tBAR%d: 0x%08lx\n", i, header0->bars[i]);
+        }
+
+        mprintf("\tIRQ Pin: %hhu\n", header0->irq_pin);
+        mprintf("\tIRQ Line: %hhu\n", header0->irq_line);
+        break;
+
+    default:
+        break;
+    }
+}
 
 int pcie_devices_init() {
     struct uacpi_table *table = kmalloc(sizeof(struct uacpi_table));
@@ -22,17 +52,29 @@ int pcie_devices_init() {
     struct acpi_mcfg_allocation mcfg_space;
 
     int mcfg_spaces =
-        (mcfg->hdr.length - (sizeof(mcfg->hdr) + sizeof(mcfg->rsvd))) /
-        sizeof(struct acpi_mcfg_allocation);
+        (mcfg->hdr.length - 0x2c) / sizeof(struct acpi_mcfg_allocation);
 
-    debugf_debug("Found %d config spaces\n", mcfg_spaces);
+    debugf_debug("Found %d config space%s\n", mcfg_spaces,
+                 mcfg_spaces == 1 ? "" : "s");
 
+    debugf(ANSI_COLOR_GRAY);
     for (int i = 0; i < mcfg_spaces; i++) {
         mcfg_space = mcfg->entries[i];
-        debugf_debug("PCI device %d BASE_ADDR: %llx; SEGMENT: %hu; BUS_RANGE: "
-                     "%hhu - %hhu\n",
-                     i + 1, mcfg_space.address, mcfg_space.segment,
-                     mcfg_space.start_bus, mcfg_space.end_bus);
+        mprintf("PCIe device n.%d\n"
+                "\tBASE_ADDR: %llx; SEGMENT: %hu; BUS_RANGE: "
+                "%hhu - %hhu\n",
+                i + 1, mcfg_space.address, mcfg_space.segment,
+                mcfg_space.start_bus, mcfg_space.end_bus);
+
+        // we should map the base address
+        // according to the spec, each device is 4K long
+        map_region_to_page(_get_pml4(), mcfg_space.address,
+                           PHYS_TO_VIRTUAL(mcfg_space.address), 0x1000,
+                           PMLE_KERNEL_READ_WRITE);
+
+        debugf(ANSI_COLOR_RESET);
+
+        dump_pcie_info((void *)PHYS_TO_VIRTUAL(mcfg_space.address));
     }
 
     kfree(table);
--- src/arch/x86_64/pcie/pcie.h
+++ src/arch/x86_64/pcie/pcie.h
@@ -4,6 +4,49 @@
 #include <stddef.h>
 #include <stdint.h>
 
+typedef enum {
+    PCIE_HEADER_T0 = 0,
+    PCIE_HEADER_T1 = 1
+} pcie_header_type;
+
+typedef struct {
+    uint16_t vendor_id;
+    uint16_t device_id;
+
+    uint16_t status_reg;
+    uint16_t command_reg;
+
+    uint16_t classcode_hi;
+    uint8_t classcode_lo;
+
+    uint8_t revision_id;
+
+    uint8_t bist; // what the heck is this
+
+    uint8_t header_type;
+
+    uint8_t plt; // Primary Latency Timer
+    uint8_t cache_line_size;
+} pcie_header_t;
+
+typedef struct {
+    uint32_t bars[6];
+
+    uint32_t reserved0;
+
+    uint16_t subsystem_id;
+    uint16_t subsystem_vendor_id;
+
+    char reserved1[7];
+
+    uint8_t capabilities_ptr;
+
+    uint16_t reserved2[6]; // we don't need MAX_LAT not MAX_GNT
+
+    uint8_t irq_pin;
+    uint8_t irq_line;
+} pcie_header0_t;
+
 int pcie_devices_init();
 
 #endif
