3903094254104b21033b5a29b8456d3002642572
Omar <76620155+RepubblicaTech@users.noreply.github.com>
2025-06-19 21:53:13 +0200

    i think strncmp is supposed to copy n bytes
    no need for checking if source string has null character (i think)

--- src/kernel/system/string.c
+++ src/kernel/system/string.c
@@ -129,13 +129,11 @@ void *memchr(const void *str, int c, size_t n) {
 char *strncpy(char *dest, const char *src, size_t n) {
     size_t i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++) {
+    for (i = 0; i < n; i++) {
         dest[i] = src[i];
     }
 
-    for (; i < n; i++) {
-        dest[i] = '\0';
-    }
+    dest[n - 1] = '\0';
 
     return dest;
 }
